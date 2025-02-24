2c8ed2e38252c43f2289a756197c9c3c63cd5c28
Dactry <development@sgd.com.au>
2025-02-24 12:09:09 +0200

    Version: 5.7.38

--- index.php
+++ index.php
@@ -17,6 +17,31 @@ if (in_array($post_type, ['industry', 'merchandise']) && is_post_type_archive($p
 	<div class="bg-primary text-white">
 
 		<div class="container section-py">
+			<?php
+			if (is_home() || is_category()):
+				$blog_page_link = get_permalink(get_option('page_for_posts'));
+				$categories = get_categories();
+			?>
+
+				<ul class="menu-row">
+					<li class="menu-row__item">
+						<a href="<?php echo esc_url($blog_page_link); ?>" class="menu-row__link<?= !is_category() ? ' active' : null; ?>">
+							All
+						</a>
+					</li>
+
+					<?php foreach ($categories as $category):
+						$category_link = get_category_link($category->term_id);
+					?>
+						<li class="menu-row__item">
+							<a href="<?php echo esc_url($category_link); ?>" class="menu-row__link<?= is_category($category->term_id) ? ' active' : null ?>">
+								<?php echo esc_html($category->name); ?>
+							</a>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			<?php endif ?>
+
 			<?php if (have_posts()) : ?>
 				<div class="row gx-3 gy-4 row-cols-1 row-cols-sm-2 row-cols-lg-3">
 					<?php while (have_posts()) : the_post(); ?>
