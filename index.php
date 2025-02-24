8ebf7aad55bdb2ca032b11b74d42afbca598ae4f
Dactry <development@sgd.com.au>
2025-02-24 12:42:47 +0200

    Version: 5.7.39

--- index.php
+++ index.php
@@ -16,43 +16,54 @@ if (in_array($post_type, ['industry', 'merchandise']) && is_post_type_archive($p
 ?>
 	<div class="bg-primary text-white">
 
-		<div class="container section-py">
+		<div class="section-py">
 			<?php
 			if (is_home() || is_category()):
 				$blog_page_link = get_permalink(get_option('page_for_posts'));
 				$categories = get_categories();
 			?>
 
-				<ul class="menu-row">
-					<li class="menu-row__item">
-						<a href="<?php echo esc_url($blog_page_link); ?>" class="menu-row__link<?= !is_category() ? ' active' : null; ?>">
-							All
-						</a>
-					</li>
-
-					<?php foreach ($categories as $category):
-						$category_link = get_category_link($category->term_id);
-					?>
+				<div class="container-fluid">
+					<ul class="menu-row">
 						<li class="menu-row__item">
-							<a href="<?php echo esc_url($category_link); ?>" class="menu-row__link<?= is_category($category->term_id) ? ' active' : null ?>">
-								<?php echo esc_html($category->name); ?>
+							<a href="<?php echo esc_url($blog_page_link); ?>" class="menu-row__link<?= !is_category() ? ' active' : null; ?>">
+								All
 							</a>
 						</li>
-					<?php endforeach; ?>
-				</ul>
+
+						<?php foreach ($categories as $category):
+							$category_link = get_category_link($category->term_id);
+						?>
+							<li class="menu-row__item">
+								<a href="<?php echo esc_url($category_link); ?>" class="menu-row__link<?= is_category($category->term_id) ? ' active' : null ?>">
+									<?php echo esc_html($category->name); ?>
+								</a>
+							</li>
+							<li class="menu-row__item">
+								<a href="<?php echo esc_url($category_link); ?>" class="menu-row__link<?= is_category($category->term_id) ? ' active' : null ?>">
+									<?php echo esc_html($category->name); ?>
+								</a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
 			<?php endif ?>
 
 			<?php if (have_posts()) : ?>
-				<div class="row gx-3 gy-4 row-cols-1 row-cols-sm-2 row-cols-lg-3">
-					<?php while (have_posts()) : the_post(); ?>
-						<div class="col" data-animate>
-							<?php get_template_part('components/card-post'); ?>
-						</div>
-					<?php endwhile; ?>
+				<div class="container">
+					<div class="row gx-3 gy-4 row-cols-1 row-cols-sm-2 row-cols-lg-3">
+						<?php while (have_posts()) : the_post(); ?>
+							<div class="col" data-animate>
+								<?php get_template_part('components/card-post'); ?>
+							</div>
+						<?php endwhile; ?>
+					</div>
 				</div>
 				<?php get_template_part('components/pagination'); ?>
 			<?php else : ?>
-				<h2>Nothing Found</h2>
+				<div class="container">
+					<h2>Nothing Found</h2>
+				</div>
 			<?php endif; ?>
 		</div>
 	</div>
